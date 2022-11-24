e8744656c9f7a30963cee1dc812d3f02183a1eed
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-11-24 16:06:39 -0500

    Fix bug with phone validation and minor button color change

--- src/components/ContactInfo/ContactInfo.js
+++ src/components/ContactInfo/ContactInfo.js
@@ -19,7 +19,7 @@ const PHONE_ERRORS = Object.freeze({
   isInvalid: 'Invalid format (Example: 123-456-7890)',
 });
 
-const PHONE_REGEX = /(\d{3})-\d{3}-\d{4}/;
+const PHONE_REGEX = /^(\d{3})-\d{3}-\d{4}$/;
 
 const EMAIL_REGEX =
   /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
