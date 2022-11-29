32c8205e6c00692ee19e083dd4035c22ceb3dcad
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-11-28 19:32:39 -0500

    Convert EducationInfoForm.js to function component

--- src/components/EducationInfoForm/EducationInfoForm.js
+++ src/components/EducationInfoForm/EducationInfoForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './EducationInfoForm.css';
 
@@ -18,240 +18,224 @@ const END_DATE_ERRORS = Object.freeze({
   isEmpty: 'End date is required',
 });
 
-export default class EducationInfoForm extends Component {
-  constructor(props) {
-    super(props);
+export default function EducationInfoForm(props) {
+  const currentDate = new Date().toISOString();
 
-    const currentDate = new Date().toISOString();
+  const [fields, setFields] = useState({
+    school: '',
+    degree: '',
+    startDate: currentDate.substring(0, currentDate.indexOf('T')),
+    endDate: currentDate.substring(0, currentDate.indexOf('T')),
+  });
 
-    this.state = {
-      school: '',
-      degree: '',
-      startDate: currentDate.substring(0, currentDate.indexOf('T')),
-      endDate: currentDate.substring(0, currentDate.indexOf('T')),
-      isEditing: true,
-      inputErrors: {
-        school: '',
-        degree: '',
-        startDate: '',
-        endDate: '',
-      },
-    };
+  const [isEditing, setIsEditing] = useState(true);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleDeleteBtn = this.handleDeleteBtn.bind(this);
-    this.handleUpdateBtn = this.handleUpdateBtn.bind(this);
-  }
+  const [inputErrors, setInputErrors] = useState({
+    school: '',
+    degree: '',
+    startDate: '',
+    endDate: '',
+  });
 
-  handleChange(e) {
-    this.setState((prevState) => ({
-      ...prevState,
+  const classNames = {
+    school: 'education-info__input education-info__input_school',
+    degree: 'education-info__input education-info__input_degree',
+    startDate: 'education-info__input education-info__input_start-date',
+    endDate: 'education-info__input education-info__input_end-date',
+  };
+
+  // Adds an error class to any field that needs it
+  if (inputErrors.school) classNames.school += ' education-info__input_error';
+  if (inputErrors.degree) classNames.degree += ' education-info__input_error';
+  if (inputErrors.startDate) classNames.startDate += ' education-info__input_error';
+  if (inputErrors.endDate) classNames.endDate += ' education-info__input_error';
+
+  /**
+   * Event handler for input field changes.
+   * @param {SyntheticEvent} e
+   */
+  function handleChange(e) {
+    setFields({
+      ...fields,
       [e.target.name]: e.target.value,
-      inputErrors: {
-        ...prevState.inputErrors,
-        [e.target.name]: '',
-      },
-    }));
+    });
+
+    setInputErrors({
+      ...inputErrors,
+      [e.target.name]: '',
+    });
   }
 
-  handleDeleteBtn() {
-    const { removeEducation, id } = this.props;
+  /**
+   * Event handler for deleting an education.
+   */
+  function handleDeleteBtn() {
+    const { removeEducation, id } = props;
     removeEducation(id);
   }
 
-  handleUpdateBtn() {
-    this.setState((prevState) => {
-      const { isEditing } = prevState;
-      let isEditingCopy = isEditing;
+  /**
+   * Event handler for saving/editing an education.
+   */
+  function handleUpdateBtn() {
+    const newInputErrors = { ...inputErrors };
+    let newIsEditing = isEditing;
+    let hasErrors = false;
 
-      if (!isEditingCopy) {
-        // Switch to edit mode
-        return { isEditing: !isEditingCopy };
-      }
-
-      // Reaching this point means save button was clicked
-      // Set up input validation
-      const { school, degree, startDate, endDate, inputErrors } = prevState;
-      const inputErrorsCopy = { ...inputErrors };
-      const inputs = Object.entries({ school, degree, startDate, endDate });
-      let hasErrors = false;
-
-      // Input validation begins
-      inputs.forEach(([input, value]) => {
-        if (input === 'school') {
+    if (isEditing) {
+      Object.entries(fields).forEach(([field, value]) => {
+        if (field === 'school') {
           // School name validation
           if (!value) {
-            inputErrorsCopy[input] = SCHOOL_ERRORS.isEmpty;
+            newInputErrors[field] = SCHOOL_ERRORS.isEmpty;
             hasErrors = true;
           }
-        } else if (input === 'degree') {
+        } else if (field === 'degree') {
           // Degree name validation
           if (!value) {
-            inputErrorsCopy[input] = DEGREE_ERRORS.isEmpty;
+            newInputErrors[field] = DEGREE_ERRORS.isEmpty;
             hasErrors = true;
           }
-        } else if (input === 'startDate') {
+        } else if (field === 'startDate') {
           // Start date validation
           if (!value) {
-            inputErrorsCopy[input] = START_DATE_ERRORS.isEmpty;
+            newInputErrors[field] = START_DATE_ERRORS.isEmpty;
             hasErrors = true;
           }
-        } else if (input === 'endDate') {
+        } else if (field === 'endDate') {
           // End date validation
           if (!value) {
-            inputErrorsCopy[input] = END_DATE_ERRORS.isEmpty;
+            newInputErrors[field] = END_DATE_ERRORS.isEmpty;
             hasErrors = true;
           }
         }
       });
 
-      if (!hasErrors) {
-        isEditingCopy = false;
-      }
+      if (!hasErrors) newIsEditing = false;
+    } else {
+      newIsEditing = true;
+    }
 
-      return { isEditing: isEditingCopy, inputErrors: inputErrorsCopy };
-    });
+    setInputErrors(newInputErrors);
+    setIsEditing(newIsEditing);
   }
 
-  render() {
-    const { school, degree, startDate, endDate, isEditing, inputErrors } = this.state;
-
-    const classNames = {
-      school: 'education-info__input education-info__input_school',
-      degree: 'education-info__input education-info__input_degree',
-      startDate: 'education-info__input education-info__input_start-date',
-      endDate: 'education-info__input education-info__input_end-date',
-    };
-
-    if (inputErrors.school) classNames.school += ' education-info__input_error';
-    if (inputErrors.degree) classNames.degree += ' education-info__input_error';
-    if (inputErrors.startDate) classNames.startDate += ' education-info__input_error';
-    if (inputErrors.endDate) classNames.endDate += ' education-info__input_error';
-
-    return (
-      <form className="education-info__form">
-        {/* School info */}
-        <div className="education-info__wrapper education-info__wrapper_school">
-          <label className="education-info__label education-info__label_school">
-            School
-            {isEditing ? (
-              <input
-                className={classNames.school}
-                name="school"
-                type="text"
-                value={school}
-                required
-                onChange={this.handleChange}
-                autoComplete="off"
-              />
-            ) : (
-              <span className="education-info__text-content">{school}</span>
-            )}
-          </label>
-          <div className="education-info__error education-info__error_school">
-            {inputErrors.school || null}
-          </div>
-        </div>
-
-        {/* Degree info */}
-        <div className="education-info__wrapper education-info__wrapper_degree">
-          <label className="education-info__label education-info__label_degree">
-            Degree
-            {isEditing ? (
-              <input
-                className={classNames.degree}
-                name="degree"
-                type="text"
-                value={degree}
-                required
-                onChange={this.handleChange}
-                autoComplete="off"
-              />
-            ) : (
-              <span className="education-info__text-content">{degree}</span>
-            )}
-          </label>
-          <div className="education-info__error education-info__error_degree">
-            {inputErrors.degree || null}
-          </div>
-        </div>
-
-        {/* Start date info */}
-        <div className="education-info__wrapper education-info__wrapper_start-date">
-          <label className="education-info__label education-info__label_start-date">
-            Start date
-            {isEditing ? (
-              <input
-                className={classNames.startDate}
-                name="startDate"
-                type="date"
-                value={startDate}
-                required
-                onChange={this.handleChange}
-                autoComplete="off"
-              />
-            ) : (
-              <span className="education-info__text-content">
-                {new Date(startDate).toDateString()}
-              </span>
-            )}
-          </label>
-          <div className="education-info__error education-info__error_start-date">
-            {inputErrors.startDate || null}
-          </div>
-        </div>
-
-        {/* End date info */}
-        <div className="education-info__wrapper education-info__wrapper_end-date">
-          <label className="education-info__label education-info__label_end-date">
-            End date
-            {isEditing ? (
-              <input
-                className={classNames.endDate}
-                name="endDate"
-                type="date"
-                value={endDate}
-                required
-                onChange={this.handleChange}
-                autoComplete="off"
-                min={startDate}
-              />
-            ) : (
-              <span className="education-info__text-content">
-                {new Date(endDate).toDateString()}
-              </span>
-            )}
-          </label>
-          <div className="education-info__error education-info__error_end-date">
-            {inputErrors.endDate || null}
-          </div>
-        </div>
-
-        {/* Form action bar */}
-        <div className="education-info__action-bar">
-          {/* Delete button */}
+  return (
+    <form className="education-info__form">
+      {/* School info */}
+      <div className="education-info__wrapper education-info__wrapper_school">
+        <label className="education-info__label education-info__label_school">
+          School
           {isEditing ? (
-            <button
-              className="education-info__delete-btn"
-              type="button"
-              onClick={this.handleDeleteBtn}
-            >
-              Delete
-            </button>
-          ) : null}
-
-          {/* Save/edit button */}
-          <button
-            className="education-info__update-btn"
-            type="button"
-            onClick={this.handleUpdateBtn}
-          >
-            {isEditing ? 'Save' : 'Edit'}
-          </button>
+            <input
+              className={classNames.school}
+              name="school"
+              type="text"
+              value={fields.school}
+              required
+              onChange={handleChange}
+              autoComplete="off"
+            />
+          ) : (
+            <span className="education-info__text-content">{fields.school}</span>
+          )}
+        </label>
+        <div className="education-info__error education-info__error_school">
+          {inputErrors.school || null}
         </div>
-      </form>
-    );
-  }
+      </div>
+
+      {/* Degree info */}
+      <div className="education-info__wrapper education-info__wrapper_degree">
+        <label className="education-info__label education-info__label_degree">
+          Degree
+          {isEditing ? (
+            <input
+              className={classNames.degree}
+              name="degree"
+              type="text"
+              value={fields.degree}
+              required
+              onChange={handleChange}
+              autoComplete="off"
+            />
+          ) : (
+            <span className="education-info__text-content">{fields.degree}</span>
+          )}
+        </label>
+        <div className="education-info__error education-info__error_degree">
+          {inputErrors.degree || null}
+        </div>
+      </div>
+
+      {/* Start date info */}
+      <div className="education-info__wrapper education-info__wrapper_start-date">
+        <label className="education-info__label education-info__label_start-date">
+          Start date
+          {isEditing ? (
+            <input
+              className={classNames.startDate}
+              name="startDate"
+              type="date"
+              value={fields.startDate}
+              required
+              onChange={handleChange}
+              autoComplete="off"
+              max={fields.endDate}
+            />
+          ) : (
+            <span className="education-info__text-content">
+              {new Date(fields.startDate).toDateString()}
+            </span>
+          )}
+        </label>
+        <div className="education-info__error education-info__error_start-date">
+          {inputErrors.startDate || null}
+        </div>
+      </div>
+
+      {/* End date info */}
+      <div className="education-info__wrapper education-info__wrapper_end-date">
+        <label className="education-info__label education-info__label_end-date">
+          End date
+          {isEditing ? (
+            <input
+              className={classNames.endDate}
+              name="endDate"
+              type="date"
+              value={fields.endDate}
+              required
+              onChange={handleChange}
+              autoComplete="off"
+              min={fields.startDate}
+            />
+          ) : (
+            <span className="education-info__text-content">
+              {new Date(fields.endDate).toDateString()}
+            </span>
+          )}
+        </label>
+        <div className="education-info__error education-info__error_end-date">
+          {inputErrors.endDate || null}
+        </div>
+      </div>
+
+      {/* Form action bar */}
+      <div className="education-info__action-bar">
+        {/* Delete button */}
+        {isEditing ? (
+          <button className="education-info__delete-btn" type="button" onClick={handleDeleteBtn}>
+            Delete
+          </button>
+        ) : null}
+
+        {/* Save/edit button */}
+        <button className="education-info__update-btn" type="button" onClick={handleUpdateBtn}>
+          {isEditing ? 'Save' : 'Edit'}
+        </button>
+      </div>
+    </form>
+  );
 }
 
 EducationInfoForm.propTypes = {
