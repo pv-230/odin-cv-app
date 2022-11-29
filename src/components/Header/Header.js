43677635c4779b815d808ce08e78e88381fe272c
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-11-28 19:33:39 -0500

    Convert Header.js to function component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Header.css';
 
-export default class Header extends Component {
-  render() {
-    return (
-      <header className="header">
-        <h1 className="header__title">CV Application</h1>
-      </header>
-    );
-  }
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">CV Application</h1>
+    </header>
+  );
 }
