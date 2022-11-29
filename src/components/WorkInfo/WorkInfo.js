b23d1d83d812b03115f7a9529d451477c6b24072
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-11-28 19:41:58 -0500

    Convert WorkInfo.js to function component

--- src/components/WorkInfo/WorkInfo.js
+++ src/components/WorkInfo/WorkInfo.js
@@ -1,50 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback } from 'react';
 import './WorkInfo.css';
 import WorkInfoForm from '../WorkInfoForm/WorkInfoForm';
 
-export default class WorkInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      workList: [],
-    };
-    this.handleAddWork = this.handleAddWork.bind(this);
-    this.removeWork = this.removeWork.bind(this);
-  }
+export default function WorkInfo() {
+  const [workList, setWorkList] = useState([]);
 
   /**
    * Event handler for adding a new work experience.
    */
-  handleAddWork() {
-    this.setState((prevState) => {
-      const { workList } = prevState;
-      const newWork = {
-        id: crypto.randomUUID(),
-      };
-      return { workList: [...workList, newWork] };
-    });
+  function handleAddWork() {
+    const newWork = { id: crypto.randomUUID() };
+    setWorkList([...workList, newWork]);
   }
 
-  removeWork(id) {
-    this.setState((prevState) => ({
-      ...prevState,
-      workList: prevState.workList.filter((work) => work.id !== id),
-    }));
-  }
+  /**
+   * Event handler for removing a work experience.
+   */
+  const removeWork = useCallback(
+    (id) => {
+      const newWorkList = workList.filter((work) => work.id !== id);
+      setWorkList(newWorkList);
+    },
+    [workList]
+  );
 
-  render() {
-    const { workList } = this.state;
-
-    return (
-      <div className="work-info">
-        <div className="work-info__title-bar">Work Experience</div>
-        {workList.map((work) => (
-          <WorkInfoForm key={work.id} id={work.id} removeWork={this.removeWork} />
-        ))}
-        <button className="work-info__add-btn" type="button" onClick={this.handleAddWork}>
-          Add work experience
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div className="work-info">
+      <div className="work-info__title-bar">Work Experience</div>
+      {workList.map((work) => (
+        <WorkInfoForm key={work.id} id={work.id} removeWork={removeWork} />
+      ))}
+      <button className="work-info__add-btn" type="button" onClick={handleAddWork}>
+        Add work experience
+      </button>
+    </div>
+  );
 }
